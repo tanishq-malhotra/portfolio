e1740c63323cfc4021a75835c64f5308a4bc4b1b
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-03 21:26:50 +0530

    Assets and source link added

---
+++ lib/config/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  static const fb = '../../assets/icons/facebook.png';
+  static const git = '../../assets/icons/github.png';
+  static const insta = '../../assets/icons/instagram.png';
+  static const linkedin = '../../assets/icons/linkedin.png';
+  static const moon = '../../assets/icons/moon.png';
+}
---
+++ lib/config/sources.dart
@@ -0,0 +1,5 @@
+class Sources {
+  static const insta_profile = 'https://www.instagram.com/tanishq_malhotra_/';
+  static const git_profile = 'https://github.com/tanishq-malhotra';
+  static const facebook_profile = 'https://www.facebook.com/tanishq.malhotra.16';
+}
