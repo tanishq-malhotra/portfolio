185a8c66188bb28c52216348c9c6a8d1bc4cc2f6
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-10 12:58:09 +0530

    assets updated

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,8 +1,8 @@
 class Assets {
-  static const fb = 'facebook.png';
-  static const git = 'github.png';
-  static const insta = 'instagram.png';
-  static const linkedin = 'linkedin.png';
-  static const moon = 'moon.png';
-  static const avatar = 'avatar.jpg';
+  static const fb = 'assets/icons/facebook.png';
+  static const git = 'assets/icons/github.png';
+  static const insta = 'assets/icons/instagram.png';
+  static const linkedin = 'assets/icons/linkedin.png';
+  static const moon = 'assets/icons/moon.png';
+  static const avatar = 'assets/icons/avatar.jpg';
 }
