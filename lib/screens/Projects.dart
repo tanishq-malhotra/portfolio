eee8b8b6f2effb96b57e9e3d06eb2c33343477a3
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-10 22:15:00 +0530

    projects tab working

--- lib/screens/Projects.dart
+++ lib/screens/Projects.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/responsive.dart';
+import '../config/projects.dart';
+import '../widgets/project_card.dart';
+
 class Projects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Projects");
+    return ResponsiveScreen(
+      largeScreen: GridView(
+        padding: EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
+        children: projects.map((data) => ProjectCard(data)).toList(),
+      ),
+    );
   }
 }
