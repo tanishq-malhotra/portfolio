165c779672dba8c1d7e492ca4adc95a3969a43c9
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-11 19:15:34 +0530

    project card for displaying project

--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -9,45 +9,64 @@ class ProjectCard extends StatelessWidget {
   const ProjectCard(this.project);
 
   void handleProjectTap() {
-    if(project.link.length > 0){
+    if (project.link.length > 0) {
       html.window.open(project.link, 'tanishq-malhotra');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     return Card(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
       child: InkWell(
         onTap: handleProjectTap,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Expanded(
-              flex: 40,
-              child: Image.asset(
-                project.image,
-                width: width * 0.25,
-                height: height * 0.25,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                flex: 40,
+                child: Image.asset(
+                  project.image,
+                  width: width * 0.25,
+                  height: width * 0.25,
+                ),
               ),
-            ),
-            SizedBox(
-              width: width * 0.5,
-            ),
-            Expanded(
-              flex: 60,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(project.name),
-                  Text(project.desc),
-                ],
+              Expanded(
+                flex: 3,
+                child: Container(),
               ),
-            )
-          ],
+              Expanded(
+                flex: 60,
+                child: Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Wrap(
+                    children: <Widget>[
+                      Text(
+                        project.name,
+                        style: TextStyle(
+                          fontFamily: 'GoogleSansRegular',
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.03,
+                      ),
+                      Text(
+                        project.desc,
+                        textScaleFactor: 1.2,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
