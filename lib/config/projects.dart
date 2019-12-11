ba7f29b3a4c94d2b85aefe412306b349b2045b34
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-11 19:15:44 +0530

    style changed

--- lib/config/projects.dart
+++ lib/config/projects.dart
@@ -3,9 +3,9 @@ import './assets.dart';
 
 final List<Project> projects = [
   Project(
-    name: 'Expense-Tracker',
-    image: Assets.expTrack,
-    link: 'https://github.com/tanishq-malhotra/Expense-Tracker',
-    desc: 'An Expense-Tracker for tracking weekly expenses made using Flutter. Shows a graph of expenses day wise.'
-  ),
+      name: 'Expense-Tracker',
+      image: Assets.expTrack,
+      link: 'https://github.com/tanishq-malhotra/Expense-Tracker',
+      desc:
+          'An Expense-Tracker for tracking weekly expenses made using Flutter. Shows a graph of expenses day wise.'),
 ];
