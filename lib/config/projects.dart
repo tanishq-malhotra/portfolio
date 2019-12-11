a82e5732b374e57e4fb701538e33feee1922c64c
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-12-11 21:25:03 +0530

    document scanner project added

--- lib/config/projects.dart
+++ lib/config/projects.dart
@@ -3,9 +3,15 @@ import './assets.dart';
 
 final List<Project> projects = [
   Project(
-      name: 'Expense-Tracker',
+      name: 'Expense Tracker',
       image: Assets.expTrack,
       link: 'https://github.com/tanishq-malhotra/Expense-Tracker',
       desc:
           'An Expense-Tracker for tracking weekly expenses made using Flutter. Shows a graph of expenses day wise.'),
+  Project(
+      name: 'Document Scanner',
+      image: Assets.doc,
+      link: 'https://github.com/tanishq-malhotra/Document-Scanner-OpenCV',
+      desc:
+          'Python program to scan the text document.'),
 ];
